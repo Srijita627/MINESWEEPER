4d03b18cc320f5ac21b7511f1d3c366cfab412d6
Srijita Datta <145253125+Srijita627@users.noreply.github.com>
2025-10-14 15:04:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const board = document.getElementById('board');
 let boardArray = [];
 
 // 🎵 Sounds
-const clickSound = new Audio("sounds/click.wav");
-const flagSound = new Audio("sounds/flag.mp3");
-const explosionSound = new Audio("sounds/explosion.mp3");
+const clickSound = new Audio("Sounds/click.wav");
+const flagSound = new Audio("Sounds/Flag.mp3");
+const explosionSound = new Audio("Sounds/Explosion.mp3");
 
 // 🧠 Main Initialization Function
 function initGame() {
@@ -163,3 +163,4 @@ window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 });
+
